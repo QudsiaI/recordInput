dffd6b531610d0d36b251befa34964add0b8a3f3
QudsiaI <qudsiaimtiaz1029@gmail.com>
2023-03-30 06:44:28 +0500

    Initial commit

---
+++ app/src/main/java/com/example/recordinput/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.recordinput;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button addRecord;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addRecord = (Button) findViewById(R.id.add);
+    }
+    void addNewRecord(View v){
+        Intent i=new Intent(MainActivity.this, NewRecord.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/recordinput/NewRecord.java
@@ -0,0 +1,24 @@
+package com.example.recordinput;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class NewRecord extends AppCompatActivity {
+
+    Button addBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_record);
+        addBtn = (Button) findViewById(R.id.addR);
+
+    }
+    void addRecord(View v){
+        Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
